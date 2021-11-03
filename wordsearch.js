ca2a6cf23e2fccbda11f8763e37de18e180537a7
dahir yusuf <dahir.yusuf@hotmail.com>
2021-11-03 21:13:48 +0000

    Peer programing word search

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,18 @@
 const wordSearch = (letters, word) => { 
+    let v_arr = [];
     const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+    if(!letters.length){
+        return false;
     }
+
+    for(let l=0;l<letters[0].length;l++){
+    const vertical=letters.map(ls=>ls[l]);
+    const vert=vertical.join('')
+    if (horizontalJoin[l].includes(word) || vert.includes(word)){ 
+        return true
+        }
+    }
+    return false;
 }
 
 module.exports = wordSearch
